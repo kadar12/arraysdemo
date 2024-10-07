6a89e5c67799cc830d517223a999c0c51f5a4f35
Kadar Ahmed <ka-sharif@outlook.com>
2024-10-07 13:38:58 +0100

    updated1

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+
+console.log(blogPosts);
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+console.log(blogPosts.length);
+
+blogPosts.push("my fourth blog post");
+console.log(blogPosts);
+console.log(blogPosts[3]);
